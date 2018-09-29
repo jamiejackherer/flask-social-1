856ad06eb73058592399c35a8e486a704b736bca
Daniel Lindegren <imdaniellindegren@gmail.com>
2018-09-29 09:07:38 -0700

    Add location column

--- app/users/models/user.py
+++ app/users/models/user.py
@@ -29,6 +29,7 @@ class User(UserMixin, db.Model, BaseModel):
     bio = db.Column(db.Text)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts_per_page = db.Column(db.Integer, default=10, nullable=False)
+    location = db.Column(db.String(255))
 
     full_name = column_property(first_name + " " + last_name)
     followed = db.relationship(
