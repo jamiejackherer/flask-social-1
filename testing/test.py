46bd36f861387c959079660524a11191d1fea99d
Daniel Lindegren <imdaniellindegren@gmail.com>
2018-10-01 22:14:07 -0700

    Add TestCase empty class

---
+++ testing/test.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
