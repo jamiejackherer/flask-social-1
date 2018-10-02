362680effe04d7cf2d402445e94567c0ee3a8ead
Daniel Lindegren <imdaniellindegren@gmail.com>
2018-10-01 22:07:09 -0700

    Add TestingConfig. Put back fallbacks for MAIL_USE_TLS and MAIL_PORT

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class BaseConfig:
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dlindegren']
@@ -25,6 +25,13 @@ class BaseConfig:
 
 class DefaultConfig(BaseConfig):
     DEBUG = True
+
+
+class TestingConfig:
+    DEBUG = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql://social_testing:testing@localhost/social_testing')
 
 
 class ProductionConfig(BaseConfig):
