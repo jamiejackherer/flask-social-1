de580b0a2e116df107a3bbc2b65741d36f5a039a
Daniel Lindegren <imdaniellindegren@gmail.com>
2018-10-01 20:16:14 -0700

    Define environment variables explicitly. Change ADMINS to dlindegren. Remove basedir variable

--- config.py
+++ config.py
@@ -7,24 +7,20 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 class BaseConfig:
     APP_NAME = 'social'
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'sqlite:///{}'.format(os.path.join(basedir, 'social.db'))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['onosendi']
+    ADMINS = ['dlindegren']
 
 
 class DefaultConfig(BaseConfig):
