cdb3607ffafd2ad3d040eaece045d0d7c9775e92
Daniel Lindegren <onosendi@protonmail.com>
2018-10-03 21:39:33 -0700

    Add grammar_pluralize and grammar_posession, and add comments

--- app/helpers.py
+++ app/helpers.py
@@ -14,6 +14,14 @@ def register_user(User, email, first_name, last_name, password):
 
 
 def hash_list(hash_list, hash_type='md5'):
+    """ Concat a list of text together to make a unique hash.
+       
+        Example:
+            hash_list(['one', 'two', 'three'], md5)
+
+        :param hash_list: List of text items to be concated
+        :param hash_type: Type of algorithm to hash the concated string
+    """
     string = '_'.join(map(str, hash_list)).encode('UTF-8')
     if hash_type == 'md5':
         digest = md5(string)
@@ -23,6 +31,38 @@ def hash_list(hash_list, hash_type='md5'):
 
 
 def truncate(text, max_length=150):
+    """ Truncate `text` to `max_length`.
+        
+        :param text: Text to truncate
+        :param max_length: Max length of text
+    """
     if len(text) > max_length:
         text = '{}...'.format(text[0:max_length])
+    return text
+
+
+def grammar_pluralize(text, countable):
+    """ Pluralize `text` if `countable` is not singular.
+
+        :param text: Text that should be plural, or not
+        :param countable: Integer to check if `text` should be plural
+    """
+    if countable != 1:
+        text = '{}s'.format(text)
+    return text
+
+
+def grammar_posession(text):
+    """ Grammar for posessive nouns.
+
+        Examples:
+            - John becomes John's
+            - Jess becomes Jess'
+    
+        :param: Text to form a posessive
+    """
+    if text[-1:].lower() == 's':
+        text = '{}\''.format(text)
+    else:
+        text = '{}\'s'.format(text)
     return text
