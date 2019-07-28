58eddbd651b47cefbe2cff0d1a0d928f20140a45
Daniel Lindegren <onosendi@protonmail.com>
2019-07-27 20:31:57 -0700

    Fix systemd service and virtual env

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo systemctl stop uwsgi
-source env/bin/activate
+uwsgi=uwsgi@social.service
+
+sudo systemctl stop $uwsgi
+source venv/bin/activate
 pip install -r requirements.txt
 flask db upgrade
-sudo systemctl start uwsgi
+sudo systemctl start $uwsgi
